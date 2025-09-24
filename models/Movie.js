a65ee7e66f61bb8083519d9e265bed769ac67974
Kaustav Ray <kkray1345@gmail.com>
2025-09-24 15:04:16 +0530

    Create Movie.js

---
+++ models/Movie.js
@@ -0,0 +1,15 @@
+// © 2025 Kaustav Ray. All rights reserved.
+// Licensed under the MIT License.
+
+import mongoose from "mongoose";
+
+const MovieSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    thumbnail: { type: String, required: true },
+    link: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Movie || mongoose.model("Movie", MovieSchema);
