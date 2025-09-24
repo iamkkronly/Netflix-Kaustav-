60f3f02895e4cfbd3938a9c53ff10880e8cfa5da
Kaustav Ray <kkray1345@gmail.com>
2025-09-24 16:33:18 +0530

    Update route.js

--- app/api/movies/route.js
+++ app/api/movies/route.js
@@ -1,24 +1,17 @@
-// © 2025 Kaustav Ray. All rights reserved.
-// Licensed under the MIT License.
-
 import { connectDB } from "@/lib/mongodb";
 import Movie from "@/models/Movie";
 import { NextResponse } from "next/server";
 
-// GET: Fetch all movies
-export async function GET() {
+/**
+ * DELETE /api/movies/:id
+ * Deletes a movie by ID.
+ */
+export async function DELETE(req, { params }) {
   await connectDB();
-  const movies = await Movie.find().sort({ createdAt: -1 });
-  return NextResponse.json(movies);
-}
-
-// POST: Add new movie
-export async function POST(req) {
   try {
-    await connectDB();
-    const data = await req.json();
-    const movie = await Movie.create(data);
-    return NextResponse.json(movie, { status: 201 });
+    const { id } = params;
+    await Movie.findByIdAndDelete(id);
+    return NextResponse.json({ success: true });
   } catch (err) {
     return NextResponse.json({ error: err.message }, { status: 400 });
   }
