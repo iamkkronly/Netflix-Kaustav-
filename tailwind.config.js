9b2bedd4c5955dabc7a087b5434e6f8dcbfb3354
Kaustav Ray <kkray1345@gmail.com>
2025-09-24 15:13:02 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// © 2025 Kaustav Ray. All rights reserved.
+// Licensed under the MIT License.
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
